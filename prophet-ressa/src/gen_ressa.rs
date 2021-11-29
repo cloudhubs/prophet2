f1c532538cd280f2848b073c6da412b5e86a13ab
MicahSchiewe <60760216+MicahSchiewe@users.noreply.github.com>
2021-11-29 00:39:03 -0600

    feat(prophet-ressa): Add crate to interface with ReSSA (#14)
    * Start creating ReSSA interface
    
    * Add prophet-ressa functionality
    
    * Make requested changes
    
    * Try to fix Github
    
    * Replace `map_err` with `?`

---
+++ prophet-ressa/src/gen_ressa.rs
@@ -0,0 +1,83 @@
+use prophet_ressa_minify::try_minify_ressa;
+use source_code_parser::ressa::NodePattern;
+use source_code_parser::{ressa::Indexable, Language, ModuleComponent};
+use std::collections::HashSet;
+use std::fs::DirEntry;
+use std::path::Path;
+
+use crate::Error;
+
+/// Use the langages in the provided LAAST to determine and load the needed ReSSAs
+pub fn extract_ressas(
+    ast: &[ModuleComponent],
+    ressa_dir: &Path,
+) -> Result<Vec<NodePattern>, Error> {
+    // Get subdirectories
+    let dirs = get_subdirs(ressa_dir)?;
+
+    // Find languages
+    let langs = ast
+        .iter()
+        .flat_map(|module| find_languages(module as &dyn Indexable))
+        .collect::<HashSet<Language>>();
+
+    // Create ReSSAs for the languages in the project
+    let mut ressas = vec![];
+    let minified_ressas = dirs
+        .into_iter()
+        .flat_map(|entry| {
+            let name = entry.file_name();
+            let name = name.to_str()?;
+            let lang = to_lang(name)?;
+            Some((name.to_string(), entry.path().is_dir(), lang))
+        })
+        .flat_map(|(name, is_dir, lang)| {
+            if langs.contains(&lang) && is_dir {
+                return Some(try_minify_ressa(name));
+            } else if !is_dir {
+                tracing::warn!("{:?} not a directory, cannot add {:?} ReSSA", name, lang);
+            }
+            None
+        });
+
+    // Verify no errors, abort if error
+    for entry in minified_ressas {
+        ressas.push(entry?);
+    }
+
+    // Flatten into one vector and return
+    Ok(ressas.into_iter().flatten().collect())
+}
+
+/// Retrieve the subdirectories of the directory named by the provided string
+fn get_subdirs(ressa_dir: &Path) -> Result<Vec<DirEntry>, Error> {
+    // Validate can check provided directory
+    let read_dir = std::fs::read_dir(ressa_dir)?;
+
+    // Parse and return subdirectories
+    let mut dirs = vec![];
+    for dir in read_dir {
+        dirs.push(dir?);
+    }
+    Ok(dirs)
+}
+
+/// Extract the set of all languages from the LAAST
+fn find_languages(ast: &dyn Indexable) -> HashSet<Language> {
+    let mut langs: HashSet<Language> = ast
+        .get_children()
+        .into_iter()
+        .flat_map(|child| find_languages(child))
+        .collect();
+    langs.insert(ast.get_language());
+    langs
+}
+
+// Convert to an enum describing the language the string describes
+/// (unknown being coerced to None)
+fn to_lang(string: &str) -> Option<Language> {
+    match string.to_string().into() {
+        Language::Unknown => None,
+        string => Some(string),
+    }
+}
--- prophet-ressa/src/lib.rs
+++ prophet-ressa/src/lib.rs
@@ -1,8 +1,36 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+mod gen_ressa;
+
+use std::path::Path;
+
+use gen_ressa::*;
+use source_code_parser::{
+    ressa::{run_ressa_parse, RessaResult},
+    ModuleComponent,
+};
+
+/// Errors that arise while running the ReSSA
+#[derive(Debug, Clone, thiserror::Error)]
+pub enum Error {
+    #[error("IO Error: {0}")]
+    Io(String),
+    #[error("Minify Error: {0}")]
+    Minify(String),
+}
+
+impl From<std::io::Error> for Error {
+    fn from(io_error: std::io::Error) -> Self {
+        Error::Io(io_error.to_string())
     }
+}
+
+impl From<prophet_ressa_minify::MinifyError> for Error {
+    fn from(min_error: prophet_ressa_minify::MinifyError) -> Self {
+        Error::Minify(min_error.to_string())
+    }
+}
+
+/// Run ressas in the described directory against the provided LAAST
+pub fn run_ressa(ast: &mut Vec<ModuleComponent>, ressa_dir: &Path) -> Result<RessaResult, Error> {
+    let ressas = extract_ressas(ast, ressa_dir)?;
+    Ok(run_ressa_parse(ast, ressas))
 }
