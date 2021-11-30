78bc19c5941911b1505befeb204a3e687f3b0ba5
nnazo <jacobcurtis2266@gmail.com>
2021-11-30 13:32:31 -0600

    add try_new method for EntityGraph

--- prophet-model/src/lib.rs
+++ prophet-model/src/lib.rs
@@ -81,7 +81,7 @@ impl MicroserviceGraph {
 
         // Create the graph with the service nodes
         let mut graph: DiGraph<Microservice, MicroserviceCall> = DiGraph::new();
-        let indices = MicroserviceGraph::add_nodes(&mut graph, &services);
+        let indices = add_nodes(&mut graph, &services);
 
         // Get the calls each of the services makes
         let services = services.iter().flat_map(|service| {
@@ -112,21 +112,35 @@ impl MicroserviceGraph {
 
         Some(MicroserviceGraph(graph))
     }
+}
 
-    fn add_nodes(
-        graph: &mut DiGraph<Microservice, MicroserviceCall>,
-        services: &[BTreeMap<String, Value>],
-    ) -> Vec<NodeIndex> {
-        services
-            .iter()
-            .flat_map(Microservice::try_from)
-            .map(|node| graph.add_node(node))
-            .collect::<Vec<_>>()
+fn add_nodes<'a, N, E>(
+    graph: &mut DiGraph<N, E>,
+    services: &'a [BTreeMap<String, Value>],
+) -> Vec<NodeIndex>
+where
+    N: TryFrom<&'a BTreeMap<String, Value>>,
+{
+    add_nodes_inner(graph, services.iter().flat_map(N::try_from))
+}
+
+fn add_nodes_inner<'a, N, E>(
+    graph: &mut DiGraph<N, E>,
+    services: impl Iterator<Item = N>,
+) -> Vec<NodeIndex> {
+    services
+        .map(|node| graph.add_node(node))
+        .collect::<Vec<_>>()
+}
+
+impl AsRef<DiGraph<Microservice, MicroserviceCall>> for MicroserviceGraph {
+    fn as_ref(&self) -> &DiGraph<Microservice, MicroserviceCall> {
+        &self.0
     }
 }
 
 /// Represents an entity from the ReSSA
-#[derive(Debug, PartialEq, Eq, Hash)]
+#[derive(Debug, PartialEq, Eq, Hash, Clone)]
 pub struct Entity {
     pub name: String,
     pub fields: Vec<Field>,
@@ -151,7 +165,7 @@ impl TryFrom<&BTreeMap<String, Value>> for Entity {
     }
 }
 
-#[derive(Debug, PartialEq, Eq, Hash)]
+#[derive(Debug, PartialEq, Eq, Hash, Clone)]
 pub enum DatabaseType {
     MySQL,
     MongoDB,
@@ -168,7 +182,7 @@ impl From<String> for DatabaseType {
     }
 }
 
-#[derive(Debug, PartialEq, Eq, Hash)]
+#[derive(Debug, PartialEq, Eq, Hash, Clone)]
 pub struct Field {
     pub name: String,
     pub ty: String,
@@ -185,21 +199,56 @@ impl TryFrom<&BTreeMap<String, Value>> for Field {
 }
 
 #[derive(Debug)]
-pub enum Multiplicity {
-    // ...
+pub enum Cardinality {
+    One,
+    Many,
 }
 
-#[derive(Debug)]
-pub struct EntityGraph(DiGraph<Entity, Multiplicity>);
-
-impl From<&[Entity]> for EntityGraph {
-    fn from(_entities: &[Entity]) -> Self {
-        todo!()
+impl ToString for Cardinality {
+    fn to_string(&self) -> String {
+        use Cardinality::*;
+        match self {
+            One => "1",
+            Many => "*",
+        }
+        .to_string()
     }
 }
 
-impl AsRef<DiGraph<Entity, Multiplicity>> for EntityGraph {
-    fn as_ref(&self) -> &DiGraph<Entity, Multiplicity> {
+#[derive(Debug)]
+pub struct EntityGraph(DiGraph<Entity, Cardinality>);
+
+impl EntityGraph {
+    /// Attempts to create an entity graph from a list of combined Entities
+    pub fn try_new(entities: &[Entity]) -> Option<EntityGraph> {
+        let mut graph = DiGraph::new();
+        let indices = add_nodes_inner(&mut graph, entities.iter().cloned());
+
+        // Add entity nodes to the graph
+        for entity in entities {
+            let entity_ndx = indices
+                .iter()
+                .find(|ndx| graph[**ndx].name == entity.name)?;
+
+            for field in entity.fields.iter() {
+                // Until we decide how to represent one-to-many in the ReSSA will only consider one-to-one here
+                let other_entity_ndx = indices.iter().find(|ndx| graph[**ndx].name == field.name);
+                let other_entity_ndx = match other_entity_ndx {
+                    Some(ndx) => ndx,
+                    _ => continue,
+                };
+
+                graph.add_edge(*entity_ndx, *other_entity_ndx, Cardinality::One);
+                graph.add_edge(*other_entity_ndx, *entity_ndx, Cardinality::One);
+            }
+        }
+
+        Some(EntityGraph(graph))
+    }
+}
+
+impl AsRef<DiGraph<Entity, Cardinality>> for EntityGraph {
+    fn as_ref(&self) -> &DiGraph<Entity, Cardinality> {
         &self.0
     }
 }
